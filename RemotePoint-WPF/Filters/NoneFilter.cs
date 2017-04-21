0b805e01b83fdfa765cd56ec955b57f0501b9177
cqb <38667715@qq.com>
2017-04-21 22:18:55 +0800

    nonefilter

---
+++ RemotePoint-WPF/Filters/NoneFilter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tsinghua.Kinect.RemotePoint
+{
+    class NoneFilter : Filter
+    {
+        private double data;
+
+        public NoneFilter(int n)
+        {
+
+        }
+
+        public void SetValue(double newData)
+        {
+            data = newData;
+        }
+        public double GetValue()
+        {
+            return data;
+        }
+    }
+}
